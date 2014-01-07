28f4ca8cfb126d6dc5b51cb548d21a725f98566e
chaithraVeeresh <35chaithra91@gmail.com>
2014-01-07 15:04:54 +0530

    added another slog

--- sampleXcode/sampleXcode/ViewController.m
+++ sampleXcode/sampleXcode/ViewController.m
@@ -18,6 +18,7 @@
 {
     [super viewDidLoad];
     NSLog(@"sample nslog");
+    NSLog(@"pull request");
 //   sample branch comment
 //    added new comment
 	// Do any additional setup after loading the view, typically from a nib.
